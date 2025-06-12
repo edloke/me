220b6992e41b533da129ee857b1b592d48c1d741
edloke <36289983+edloke@users.noreply.github.com>
2025-06-13 03:55:17 +0800

    Website Update 3.1.0 - New Project Layout

---
+++ js/carousel.js
@@ -0,0 +1,22 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let horizontalSection = document.querySelector('.horizontal');
+
+console.log(horizontalSection.scrollWidth);
+
+gsap.to('.horizontal', {
+  x: () => horizontalSection.scrollWidth * -1,
+  xPercent: 100,
+  scrollTrigger: {
+    trigger: '.horizontal',
+    start: 'center center',
+    end: '+=3000px',
+    pinSpacing: false,
+    pin: '#horizontal-scoll',
+    scrub: true,
+    invalidateOnRefresh: true
+    
+  }
+  
+});
+
